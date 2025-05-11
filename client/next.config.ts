f931ee0349806988454caf1fd1aea341da776841
Jan <janekkamon@gmail.com>
2025-05-11 13:44:44 +0200

    fix: config next & ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  experimental: {
+    turbo: {
+      root: "..",
+      resolveAlias: {
+        "@shared": "../shared/*",
+      },
+    },
+  },
 };
 
 export default nextConfig;
