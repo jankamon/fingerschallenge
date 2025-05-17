bc2ec5c70b9c0a5422488ae3637d3abd3f86fcec
Jan <janekkamon@gmail.com>
2025-05-17 09:43:57 +0200

    feat: correct chest level descriptions and pattern lengths in generateChestUnlockPattern function

--- server/src/utilities/generateChestUnlockPattern.ts
+++ server/src/utilities/generateChestUnlockPattern.ts
@@ -1,15 +1,15 @@
-// Returns array of 0, 1 basing on the chest level (0-3)
-// 1 level have length 3
-// 2 level have length 5
-// 3 level have length 8
-// 4 level have length 12
+// Returns array of 0, 1 basing on the chest level (1-4)
+// 1 level have length 2
+// 2 level have length 3
+// 3 level have length 5
+// 4 level have length 8
 export default function (chestLevel: number): number[] {
   if (chestLevel < 1 || chestLevel > 4) {
     throw new Error("Chest level must be between 1 and 4");
   }
 
   const patternLength =
-    chestLevel === 1 ? 3 : chestLevel === 2 ? 5 : chestLevel === 3 ? 8 : 12;
+    chestLevel === 1 ? 2 : chestLevel === 2 ? 3 : chestLevel === 3 ? 5 : 8;
 
   const pattern: number[] = [];
 
