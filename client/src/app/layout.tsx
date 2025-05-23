37f4b2e069fb55fb45b1b846b0802309977136ef
Jan <janekkamon@gmail.com>
2025-05-23 15:09:14 +0200

    Refactor code structure for improved readability and maintainability

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,11 +2,16 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "./providers";
 import Navigation from "@/components/Navigation";
+import localFont from "next/font/local";
 
 export const metadata: Metadata = {
   title: "Gothic Chest",
   description: "Think you're slicker than Fingers?",
 };
+
+const gothicFont = localFont({
+  src: "./GothicIINocKruka.ttf",
+});
 
 export default function RootLayout({
   children,
@@ -15,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="flex flex-col w-full h-screen antialiased">
+      <body
+        className={`flex flex-col w-full h-screen ${gothicFont.className} antialiased`}
+      >
         <Navigation />
         <main className="flex-1 w-full md:w-3/4 lg:w-2/5 mx-auto">
           <Providers>{children}</Providers>
