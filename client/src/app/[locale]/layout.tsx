b0e30b84ef0b785c77896669bde623b12298f27e
Jan <janekkamon@gmail.com>
2025-07-03 11:25:25 +0200

    fix: update image URLs in generateMetadata function to use baseUrl

--- client/src/app/[locale]/layout.tsx
+++ client/src/app/[locale]/layout.tsx
@@ -29,7 +29,7 @@ export async function generateMetadata({
       type: "website",
       images: [
         {
-          url: "/assets/logo/cover.jpg",
+          url: `${baseUrl}/assets/logo/cover.jpg`,
           width: 1200,
           height: 630,
           alt: t("title"),
@@ -40,7 +40,7 @@ export async function generateMetadata({
       card: "summary_large_image",
       title: t("title"),
       description: t("description"),
-      images: ["/assets/logo/cover.jpg"],
+      images: [`${baseUrl}/assets/logo/cover.jpg`],
       creator: "@JanKamonPL",
     },
   };
